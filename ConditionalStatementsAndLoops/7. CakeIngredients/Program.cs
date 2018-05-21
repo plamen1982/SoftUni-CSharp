de70de134a49964deb71482f24841a1a1ce19f2c
plamen1982 <plamen.hristov@ymail.com>
2018-05-21 10:59:02 +0300

    7. Cake Ingredients

--- ConditionalStatementsAndLoops/7. CakeIngredients/Program.cs
+++ ConditionalStatementsAndLoops/7. CakeIngredients/Program.cs
@@ -6,7 +6,23 @@ namespace _7._CakeIngredients
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int count = 0;
+            string ingredient = "";
+
+            for (int i = 0; i <= 20; i++) {
+                ingredient = Console.ReadLine();
+                if (ingredient == "Bake!")
+                {
+                    Console.WriteLine($"Preparing cake with {count} ingredients.");
+                    return;
+                }
+
+                else
+                {
+                    count++;
+                    Console.WriteLine($"Adding ingredient {ingredient}.");
+                }        
+            }
         }
     }
 }
