b2f8f64e5a9d0af7cb34c54ed5143f6ab41b3e06
plamen1982 <plamen.hristov@ymail.com>
2018-05-21 12:11:13 +0300

    Count the Integers

---
+++ ConditionalStatementsAndLoops/9. Count the Integers/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _9._Count_the_Integers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int counter = 0;
+
+                try
+                {
+                    while (true)
+                    {
+                        int integerNumber = int.Parse(Console.ReadLine());
+                        counter++;
+                    }
+                }
+
+                catch (Exception)
+                {
+
+                    Console.WriteLine(counter);
+                }
+            
+        }
+    }
+}
