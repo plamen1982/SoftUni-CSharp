15aa9412ac9bebbfaab49053b93fc6569608ef8d
plamen1982 <plamen.hristov@ymail.com>
2018-05-22 08:25:15 +0300

    Data types - 1. Centuries to minutes

---
+++ DataTypesAndVariable/1. CenturiesToMinutes/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1.CenturiesToMinutes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                Console.Write("Century = ");
+                byte century = byte.Parse(Console.ReadLine());
+                ushort years = (ushort)(century * 100);
+                uint days = (uint)(years * 365.2422);
+                uint hours = days * 24;
+                uint minutes = hours * 60;
+
+                Console.WriteLine($"{century} centuries = {years} years = {days} days = {hours} hours = {minutes} minutes");
+
+            }
+        }
+    }
+}
---
+++ DataTypesAndVariable/DataTypesAndVariable/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Numerics;
+
+namespace DataTypesAndVariable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 10;
+            for (int i = 0; i < 100000000; i++)
+            {
+                Console.Write(i*100);
+                Console.Write("\r");
+            }
+        }
+    }
+}
