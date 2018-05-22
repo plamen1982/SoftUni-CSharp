4cf7cf78cc3904952fa8a43964c629ec2e2eefbe
plamen1982 <plamen.hristov@ymail.com>
2018-05-22 08:40:43 +0300

    Data types - 2. Circle area with 12 digits precisions

---
+++ DataTypesAndVariable/2. CircleArea12digitsPrecision/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2.CircleArea12digitsPrecision
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double radius = double.Parse(Console.ReadLine());
+            double circleArea = Math.PI * Math.Pow(radius, 2);
+
+            Console.WriteLine($"{circleArea:f12}");
+        }
+    }
+}
