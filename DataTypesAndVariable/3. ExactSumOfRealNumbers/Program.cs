a09a68e228ef059868fd0946925542072f23effc
plamen1982 <plamen.hristov@ymail.com>
2018-05-22 09:41:06 +0300

    Data types Task 3 and Task 4 - Lab

---
+++ DataTypesAndVariable/3. ExactSumOfRealNumbers/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3.ExactSumOfRealNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            decimal sum = 0;
+            for (int i = 0; i < n; i++)
+            {
+                
+                decimal number = decimal.Parse(Console.ReadLine());
+                sum += number;
+
+            }
+            Console.WriteLine(sum);
+            Console.Write(sum.GetType());
+        }
+    }
+}
---
+++ DataTypesAndVariable/4. Elevator/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4.Elevator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int people = int.Parse(Console.ReadLine());
+            int capacityPeople = int.Parse(Console.ReadLine());
+
+            if (capacityPeople > people)
+            {
+                Console.WriteLine(1);
+            }
+            else
+            {
+                int course = (int)Math.Ceiling((double)people / capacityPeople);
+                Console.WriteLine(course);
+
+            }
+        }
+    }
+}
