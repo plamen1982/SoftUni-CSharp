8ea0dad64eb302e0fc1bb76f7248998ffefa7b07
plamen1982 <plamen.hristov@ymail.com>
2018-05-22 10:09:05 +0300

    Data types 5. Special Numbers

---
+++ DataTypesAndVariable/5. SpecialNumbers/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5.SpecialNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= n; i++)
+            {
+                int sum = 0;
+                int currentIdex = i;
+
+                while (currentIdex > 0)
+                {
+                    sum += currentIdex % 10;
+                    currentIdex = currentIdex / 10;
+                }
+                if (sum == 5 || sum == 7 || sum == 11)
+                {
+                    Console.WriteLine($"{i} -> True");
+                }
+
+                else
+                {
+                    Console.WriteLine($"{i} -> False");
+                }
+
+            }
+        }
+    }
+}
