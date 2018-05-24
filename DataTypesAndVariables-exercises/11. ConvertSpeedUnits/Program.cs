6a820b6a82b064ad58cf860a0ef16365505f8749
plamen1982 <plamen.hristov@ymail.com>
2018-05-24 07:19:58 +0300

    Task 11 Convert Speed Units - Data types and variables

---
+++ DataTypesAndVariables-exercises/11. ConvertSpeedUnits/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace _11._ConvertSpeedUnits
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int milePerMeters = 1609;
+
+            int meters = int.Parse(Console.ReadLine());
+            int hours = int.Parse(Console.ReadLine());
+            int minutes = int.Parse(Console.ReadLine());
+            int seconds = int.Parse(Console.ReadLine());
+
+            int totalSeconds = hours * 3600 + minutes * 60 + seconds;
+            float totalHours = (float)totalSeconds / 3600;
+
+            float kilometers = (float)(meters / 1000);
+            float miles = kilometers / 1.609f;
+
+            float metersPerSecond = (float)meters / (float)totalSeconds;
+            float kilometersPerHour = kilometers / totalHours;
+            float milesPerHour = miles / totalHours;
+
+            Console.WriteLine(metersPerSecond);
+            Console.WriteLine(kilometersPerHour);
+            Console.WriteLine(milesPerHour);
+
+            /*
+             * int milesPerHour 
+            int seconds */
+
+
+
+        }
+    }
+}
