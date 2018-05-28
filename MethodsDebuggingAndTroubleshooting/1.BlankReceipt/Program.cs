cfd9d5f7ee9e269189d07399251101ec0a60e1ec
plamen1982 <plamen.hristov@ymail.com>
2018-05-28 18:47:11 +0600

    Methods - Lab: Task 1 and Task 2

---
+++ MethodsDebuggingAndTroubleshooting/1.BlankReceipt/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _1.BlankReceipt
+{
+    class Program
+    {
+        static void Header()
+        {
+            Console.WriteLine($"CASH RECEIPT{Environment.NewLine}------------------------------");
+        }
+        static void Body()
+        {
+            Console.WriteLine($"Charged to____________________{Environment.NewLine}Received by___________________");
+        }
+        static void Footer()
+        {
+            Console.WriteLine($"------------------------------{Environment.NewLine}© SoftUni");
+        }
+        static void Main(string[] args)
+        {
+            Header();
+            Body();
+            Footer();
+        }
+    }
+}
---
+++ MethodsDebuggingAndTroubleshooting/2.SignOfIntegerNumber/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _2.SignOfIntegerNumber
+{
+    class Program
+    {
+        static void PrintSign(int number)
+        {
+            if (number > 0)
+            {
+                Console.WriteLine($"The number {number} is positive.");
+            }
+            else if (number < 0)
+            {
+                Console.WriteLine($"The number {number} is negative.");
+            }
+            else
+            {
+                Console.WriteLine($"The number {number} is zero.");
+            }
+        }
+        static void Main(string[] args)
+        {
+            PrintSign(int.Parse(Console.ReadLine()));
+        }
+    }
+}
