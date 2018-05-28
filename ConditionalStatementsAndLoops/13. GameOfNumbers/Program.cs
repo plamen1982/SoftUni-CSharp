b743cf6de7b2559588d7e31bdabe6610a1b65184
plamen1982 <plamen.hristov@ymail.com>
2018-05-28 10:32:25 +0300

    13. Game of numbers - Conditional Statements and Loops

--- ConditionalStatementsAndLoops/13. GameOfNumbers/Program.cs
+++ ConditionalStatementsAndLoops/13. GameOfNumbers/Program.cs
@@ -6,7 +6,24 @@ namespace _13._GameOfNumbers
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int n = int.Parse(Console.ReadLine());
+            int m = int.Parse(Console.ReadLine());
+            int magicNumber = int.Parse(Console.ReadLine());
+            int count = 0;
+
+            for (int i = n; i <= m; i++)
+            {
+                for (int j = n; j <= m; j++)
+                {
+                    count++;
+                    if ((i + j) == magicNumber)
+                    {
+                        Console.WriteLine($"Number found! {j} + {i} = {magicNumber}");
+                        return;
+                    }
+                }
+            }
+            Console.WriteLine($"{count} combinations - neither equals {magicNumber}");
         }
     }
 }
