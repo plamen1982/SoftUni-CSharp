031d3f9704ed79539abb2452da09ef72e7a6be7c
plamen1982 <plamen.hristov@ymail.com>
2018-05-28 15:33:06 +0600

    15. Neighbor Wars - Conditional Statement

---
+++ ConditionalStatementsAndLoops/15.NeighbourWars/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+
+namespace _15.NeighbourWars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int peshoDamage = int.Parse(Console.ReadLine());
+            int goshoDamage = int.Parse(Console.ReadLine());
+
+            int peshoHealth = 100;
+            int goshoHealth = 100;
+
+            int round = 0;
+
+            while (peshoHealth > 0 && goshoHealth > 0)
+            {
+
+                round++;
+                goshoHealth -= peshoDamage;
+
+                if (goshoHealth > 0)
+                {
+                    Console.WriteLine($"Pesho used Roundhouse kick and reduced Gosho to {goshoHealth} health.");
+
+                }
+                else
+                {
+                    break;
+                }
+
+                if (round % 3 == 0)
+                {
+                    goshoHealth += 10;
+                    peshoHealth += 10;
+                }
+
+
+
+
+                round++;
+                peshoHealth -= goshoDamage;
+
+                if (peshoHealth > 0)
+                {
+                    Console.WriteLine($"Gosho used Thunderous fist and reduced Pesho to {peshoHealth} health.");
+
+                }
+                else
+                {
+                    break;
+                }
+                if (round % 3 == 0)
+                {
+                    goshoHealth += 10;
+                    peshoHealth += 10;
+                }
+
+
+            }
+
+            if (peshoHealth <= 0)
+            {
+                Console.WriteLine($"Gosho won in {round}th round.");
+            }
+            else if (goshoHealth <= 0)
+            {
+                Console.WriteLine($"Pesho won in {round}th round.");
+
+            }
+        }
+    }
+}
