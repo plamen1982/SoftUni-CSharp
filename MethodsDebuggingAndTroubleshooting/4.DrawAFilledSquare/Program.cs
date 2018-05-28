278275330e7a71183a1521884694830cc96446ed
plamen1982 <plamen.hristov@ymail.com>
2018-05-28 19:15:17 +0600

    Methods - Lab: DrawAFilledSquare

---
+++ MethodsDebuggingAndTroubleshooting/4.DrawAFilledSquare/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace _4.DrawAFilledSquare
+{
+    class Program
+    {
+        static void DrawAFilledSquare(int n)
+        {
+            Console.WriteLine(new string('-', 2 * n));
+            
+            for (int i = 0; i < n - 2; i++)
+            {
+                Console.Write('-');
+
+                for (int j = 0; j <  n - 1; j++)
+                {
+                    Console.Write("\\/");
+                }
+
+                Console.Write('-');
+                Console.WriteLine();
+
+            }
+
+            Console.WriteLine(new string('-', 2 * n));
+        }
+        static void Main(string[] args)
+        {
+            DrawAFilledSquare(int.Parse(Console.ReadLine()));
+        }
+    }
+}
