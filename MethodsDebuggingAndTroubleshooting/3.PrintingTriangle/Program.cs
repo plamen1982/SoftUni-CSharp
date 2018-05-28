6d78d1d839f07e870e9353fb791ebd761c65b91a
plamen1982 <plamen.hristov@ymail.com>
2018-05-28 18:57:01 +0600

    Methods - Lab: Printing Triangle

---
+++ MethodsDebuggingAndTroubleshooting/3.PrintingTriangle/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace _3.PrintingTriangle
+{
+    class Program
+    {
+        static void PrintingTriangle(int n)
+        {
+            for (int i = 1; i <= n; i++)
+            {
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write($"{j} ");
+                }
+                Console.WriteLine();
+            }
+            for (int i = n - 1; i >= 1; i--)
+            {
+                for (int j = 1; j <= i; j++)
+                {
+
+                    Console.Write($"{j} ");
+
+                }
+                Console.WriteLine();
+            }
+        }
+        static void Main(string[] args)
+        {
+            PrintingTriangle(int.Parse(Console.ReadLine()));
+        }
+    }
+}
