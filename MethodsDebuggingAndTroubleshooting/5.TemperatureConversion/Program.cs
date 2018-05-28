090d4c911f3d879f1971800ed6d4f019ad8900f1
plamen1982 <plamen.hristov@ymail.com>
2018-05-28 19:26:22 +0600

    Methods - Lab: 5. Temperature Conversion

---
+++ MethodsDebuggingAndTroubleshooting/5.TemperatureConversion/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _5.TemperatureConversion
+{
+    class Program
+    {
+        static double TemperatureConversion(double n)
+        {
+
+            double fahrenheit = (n - 32) * 5 / 9;
+            return fahrenheit;
+        }
+        static void Main(string[] args)
+        {
+          double celsius = TemperatureConversion(double.Parse(Console.ReadLine()));
+          Console.WriteLine($"{celsius:f2}");
+        }
+    }
+}
