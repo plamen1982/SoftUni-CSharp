ae80a6019a4e4df80cbd6c68731c113d6f2e60d3
plamen1982 <plamen.hristov@ymail.com>
2018-05-29 19:59:18 +0600

    5. Fibonacci Number - 5.Methods

---
+++ MethodsDebuggingAndTroubleshootingCode-Exercises/5.FibonacciNumbers/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _5.FibonacciNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            Console.WriteLine(FibonacciNumbers(n));
+        }
+        static long FibonacciNumbers(int n)
+        {
+            long f0 = 0;
+            long f1 = 1;
+            long f2 = 1;
+            for (int i = 0; i < n; i++)
+            {
+                f2 = f1 + f0;
+                f0 = f1;
+                f1 = f2;
+            }
+            return f2;
+        }
+    }
+}
