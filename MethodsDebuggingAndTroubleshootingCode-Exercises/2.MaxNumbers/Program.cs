e0958d8e2f6b8f5874741232f93fed787a68780c
plamen1982 <plamen.hristov@ymail.com>
2018-05-29 18:13:14 +0600

    5.Methods - 2.MaxNumbers

---
+++ MethodsDebuggingAndTroubleshootingCode-Exercises/2.MaxNumbers/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _2.MaxNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            int c = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(GetMax(a, b, c));
+        }
+
+        static int GetMax(int a, int b, int c)
+        {
+            int maxAB = Math.Max(a, b);
+            return Math.Max(maxAB, c);
+        }
+    }
+}
---
+++ MethodsDebuggingAndTroubleshootingCode-Exercises/MethodsDebuggingAndTroubleshootingCode-Exercises/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MethodsDebuggingAndTroubleshootingCode_Exercises
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
