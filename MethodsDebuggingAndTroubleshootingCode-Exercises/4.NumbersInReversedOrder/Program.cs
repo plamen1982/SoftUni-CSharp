a805df179076bbd58069d096f3f30ebbe2673cdc
plamen1982 <plamen.hristov@ymail.com>
2018-05-29 19:29:22 +0600

    4.Numbers In Reversed Order - 5.Methods

---
+++ MethodsDebuggingAndTroubleshootingCode-Exercises/4.NumbersInReversedOrder/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _4.NumbersInReversedOrder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string number = Console.ReadLine();
+            Console.WriteLine(ReveredNumber(number));
+        }
+
+        static string ReveredNumber(string number)
+        {
+            string result = "";
+            for (int i = number.Length - 1; i >= 0; i--)
+            {
+                result += number[i];
+            }
+            return result;
+        }
+
+    }
+}
