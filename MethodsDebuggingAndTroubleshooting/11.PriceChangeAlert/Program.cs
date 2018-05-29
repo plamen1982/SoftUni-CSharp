1b2d114b19384580faeeab2abc456ff53ed133d1
plamen1982 <plamen.hristov@ymail.com>
2018-05-29 12:21:40 +0600

    11.Price change alert - 4.Methods and debugging Lab

---
+++ MethodsDebuggingAndTroubleshooting/11.PriceChangeAlert/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace _11.PriceChangeAlert
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int numberOfPrices = int.Parse(Console.ReadLine()); 
+            double limitPrice = double. Parse(Console.ReadLine()); 
+            double lastPrice = double.Parse(Console.ReadLine()); 
+
+        for (int i = 0; i < numberOfPrices - 1; i++)
+            {
+
+                double currentPrice = double.Parse(Console.ReadLine()); 
+                double differenceInPrices = ProccessingPrice(lastPrice, currentPrice);
+                bool isSignificantDifference = isThereADifference(differenceInPrices, limitPrice);
+                string message = GetMessage(currentPrice, lastPrice, differenceInPrices, isSignificantDifference);
+
+                Console.WriteLine(message);
+
+                lastPrice = currentPrice;
+
+            }  
+        }
+
+        private static string GetMessage(double currentPrice, double lastPrice, double differenceInPrices, bool isSignificantDifference)
+            {
+                string message = "";
+
+                if (differenceInPrices == 0)
+                {
+                    message = string.Format("NO CHANGE: {0}", currentPrice);
+                }
+                else if (!isSignificantDifference)
+                {
+                    message = string.Format("MINOR CHANGE: {0} to {1} ({2:F2}%)", lastPrice, currentPrice, differenceInPrices*100);
+                }
+                else if (isSignificantDifference && (differenceInPrices > 0))
+                {
+                    message = string.Format("PRICE UP: {0} to {1} ({2:F2}%)", lastPrice, currentPrice, differenceInPrices*100);
+                }
+                else if (isSignificantDifference && (differenceInPrices < 0))
+                {
+                    message = string.Format("PRICE DOWN: {0} to {1} ({2:F2}%)", lastPrice, currentPrice, differenceInPrices*100);
+                }
+
+                return message;
+            }
+
+        private static bool isThereADifference(double differenceInPrices, double limitPrice)
+        {
+            if (Math.Abs(differenceInPrices) >= limitPrice)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        private static double ProccessingPrice(double lastPrice, double currentPrice)
+        {
+            double result = (currentPrice - lastPrice) / lastPrice;
+            return result;
+        }
+    }
+}
