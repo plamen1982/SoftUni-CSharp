646e42df4c3da4cf08f41c67545bf48da9c90b12
plamen1982 <plamen.hristov@ymail.com>
2018-05-30 11:04:02 +0600

    7. Center point - 5.Methods

---
+++ MethodsDebuggingAndTroubleshootingCode-Exercises/8.CenterPoint/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _8.CenterPoint
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double x1 = double.Parse(Console.ReadLine());
+            double y1 = double.Parse(Console.ReadLine());
+            double x2 = double.Parse(Console.ReadLine());
+            double y2 = double.Parse(Console.ReadLine());
+
+            GetClosestPoint(x1, y1, x2, y2);
+
+        }
+        static void GetClosestPoint(double x1, double y1, double x2, double y2)
+        {
+            double firstLine = Math.Sqrt(x1 * x1 + y1 * y1);
+            double secondLine = Math.Sqrt(x2 * x2 + y2 * y2);
+            if (firstLine > secondLine)
+            {
+                Console.WriteLine($"({x2}, {y2})");
+            }
+            else
+            {
+                Console.WriteLine($"({x1}, {y1})");
+            }
+        }
+    }
+}
