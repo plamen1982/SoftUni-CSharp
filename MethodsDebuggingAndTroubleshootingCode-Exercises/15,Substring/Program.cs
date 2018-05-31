b3160b014ad9555ad5a746e4c227f1a3519ff7c2
plamen1982 <plamen.hristov@ymail.com>
2018-05-31 11:06:41 +0600

    15. Refactoring Substring - 5.Methods

---
+++ MethodsDebuggingAndTroubleshootingCode-Exercises/15,Substring/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace _15_Substring
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+            int jump = int.Parse(Console.ReadLine());
+
+            char Search = 'p';
+            bool hasMatch = false;
+
+            for (int i = 0; i < text.Length; i++)
+            {
+
+                if (text[i] == Search)
+                {
+                    hasMatch = true;
+
+                    int endIndex = jump + 1;
+
+                    if (endIndex > text.Length)
+                    {
+                        endIndex = text.Length;
+                    }
+                    if (i + endIndex > text.Length)
+                    {
+                        endIndex = text.Length - i;
+                    }
+
+                    string matchedString = text.Substring(i, endIndex);
+                    Console.WriteLine(matchedString);
+                    i += jump;
+                }
+            }
+
+            if (!hasMatch)
+            {
+                Console.WriteLine("no");
+            }
+        }
+    }
+}
