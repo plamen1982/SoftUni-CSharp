67a9f340ba48afad3c630aa67c9514baec153275
plamen1982 <plamen.hristov@ymail.com>
2018-05-31 19:58:06 +0600

    2. Reverse Array of Int - Arrays - Lab

---
+++ Arrays/2.ReverseАnArrayОfIntegers/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _2.ReverseАnArrayОfIntegers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            int[] arrayNumbers = new int[number];
+            for (int i = 0; i < arrayNumbers.Length; i++)
+            {
+                arrayNumbers[i] = int.Parse(Console.ReadLine()); 
+            }
+
+            for (int i = arrayNumbers.Length - 1; i >= 0; i--)
+            {
+                Console.WriteLine(arrayNumbers[i]);
+            }
+        }
+    }
+}
