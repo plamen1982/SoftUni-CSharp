50ecc958391f14c24feba7322ec69a61a553d6a6
plamen1982 <plamen.hristov@ymail.com>
2018-05-31 09:46:24 +0600

    14. Factorial Trailing Zeroes - 5.Methods

---
+++ MethodsDebuggingAndTroubleshootingCode-Exercises/14.FactorialTrailingZeroes/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Numerics;
+
+namespace _14.FactorialTrailingZeroes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            BigInteger factorial = Factorial(number);
+            Console.WriteLine(CountTrailingZeros(factorial));
+        }
+
+        static int CountTrailingZeros(BigInteger factorial)
+        {
+            string factorialString = factorial.ToString();
+            int count = 0;
+
+            for (int i = factorialString.Length - 1; i > 0; i--)
+            {
+                if (factorialString[i] == '0')
+                {
+                    count++;
+                }
+                else
+                {
+                    break;
+                }
+            }
+            return count;
+        }
+    
+        static BigInteger Factorial(int number)
+        {
+            BigInteger result = 1;
+            for (int i = 1; i <= number; i++)
+            {
+                result *= i;
+            }
+            return result;
+        }
+
+    }
+}
