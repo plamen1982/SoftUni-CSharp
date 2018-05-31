743f05427c52c60c3a7e6b38aa8a623927b82b28
plamen1982 <plamen.hristov@ymail.com>
2018-05-31 18:55:20 +0600

    1.Day of week - Arrays - Lab

---
+++ Arrays/1.DayOfWeek/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace _1.DayOfWeek
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+
+            string[] dayOfWeek = 
+            {
+                "Monday",
+                "Tuesday",
+                "Wednesday",
+                "Thursday",
+                "Friday",
+                "Saturday",
+                "Sunday"
+            };
+            for (int i = 1; i <= dayOfWeek.Length; i++)
+            {
+                if (i == number)
+                {
+                    Console.WriteLine(dayOfWeek[i - 1]);
+                    return;
+                }
+            }
+            Console.WriteLine("Invalid day!");
+        }
+    }
+}
