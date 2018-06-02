7ebcc33acd207c09433bcdb4ede5dd069ada80fe
plamen1982 <plamen.hristov@ymail.com>
2018-06-02 09:58:27 +0600

    Array-Exercise - 1.Largest Common End

---
+++ Arrays-Exercise/1.LargestCommonEnd/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace _1.LargestCommonEnd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] firstArrayOfWords = Console.ReadLine().Split(" ");
+            string[] secondArrayOfWords = Console.ReadLine().Split(" ");
+            int minLength = Math.Min(firstArrayOfWords.Length, secondArrayOfWords.Length);
+            int count = 0;
+            int count1 = 0;
+            for (int i = 0; i < minLength; i++)
+            {
+
+                if (firstArrayOfWords[i] == secondArrayOfWords[i])
+                {
+                    count++;
+                }
+            }
+            for (int j = 0; j < minLength; j++)
+            {
+                if (firstArrayOfWords[firstArrayOfWords.Length - 1 - j] == secondArrayOfWords[secondArrayOfWords.Length - 1 - j])
+                {
+                    count1++;
+                }
+            }
+            int maxCount = Math.Max(count, count1);
+            Console.WriteLine(maxCount);
+        }
+    }
+}
---
+++ Arrays-Exercise/3.FoldAndSum/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+
+namespace _3.FoldAndSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbersArr = Console.ReadLine()
+                                .Split(" ")
+                                .Select(int.Parse)
+                                .ToArray();
+            int k = numbersArr.Length / 4;
+
+            int[] upperArray = new int[numbersArr.Length / 2];
+            int[] lowerArray = new int[numbersArr.Length / 2];
+
+            for (int i = 0; i < k; i++)
+            {
+                upperArray[i] = numbersArr[k - 1 - i];
+                //upperArray[4 * k - 2 + i] = numbersArr[4 * k - i - 1];
+            }
+                                
+        }
+    }
+}
