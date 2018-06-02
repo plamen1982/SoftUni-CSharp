34b3f5c30b4aea3812d8e15cbe9a742e0c098bd2
plamen1982 <plamen.hristov@ymail.com>
2018-06-02 12:07:40 +0600

    2. Rotate and sum - Arrays

--- Arrays-Exercise/2.RotateAndSum/Program.cs
+++ Arrays-Exercise/2.RotateAndSum/Program.cs
@@ -7,11 +7,35 @@ namespace _2.RotateAndSum
     {
         static void Main(string[] args)
         {
-            //TODO enter numbers of rotatations and arrays
-            //one arrya for the sum
 
-            int[] arrayOfInt = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+
+            int[] arrayOfInt = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            int[] sum = new int[arrayOfInt.Length];
             int kTimes = int.Parse(Console.ReadLine());
+            for (int i = 0; i < kTimes; i++)
+            {
+                RotatedArray(arrayOfInt);
+                for (int j = 0; j < arrayOfInt.Length; j++)
+                {
+                    sum[j] += arrayOfInt[j];
+                }
+            }
+
+            Console.WriteLine(string.Join(" ", sum));
+
+        }
+
+        static int[] RotatedArray(int[] arrayOfInt)
+        {
+            int lastIndex = arrayOfInt[arrayOfInt.Length - 1];
+
+            for (int i = 0; i < arrayOfInt.Length - 1; i++)
+            {
+                arrayOfInt[arrayOfInt.Length - 1 - i] = arrayOfInt[arrayOfInt.Length - 2 - i];
+            }
+
+            arrayOfInt[0] = lastIndex;
+            return arrayOfInt;
         }
     }
 }
