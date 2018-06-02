e368f4a70fcc87af398334f04b598d5002705f92
plamen1982 <plamen.hristov@ymail.com>
2018-06-02 10:48:41 +0600

    3. Fold and Sum - Arrays

---
+++ Arrays-Exercise/2.RotateAndSum/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+
+namespace _2.RotateAndSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //TODO enter numbers of rotatations and arrays
+            //one arrya for the sum
+
+            int[] arrayOfInt = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+            int kTimes = int.Parse(Console.ReadLine());
+        }
+    }
+}
--- Arrays-Exercise/3.FoldAndSum/Program.cs
+++ Arrays-Exercise/3.FoldAndSum/Program.cs
@@ -15,13 +15,24 @@ namespace _3.FoldAndSum
 
             int[] upperArray = new int[numbersArr.Length / 2];
             int[] lowerArray = new int[numbersArr.Length / 2];
+            int[] sum = new int[numbersArr.Length / 2];
 
             for (int i = 0; i < k; i++)
             {
                 upperArray[i] = numbersArr[k - 1 - i];
-                //upperArray[4 * k - 2 + i] = numbersArr[4 * k - i - 1];
+                upperArray[upperArray.Length - 1 - i] = numbersArr[3 * k + i];
             }
-                                
+
+            for (int j = 0; j < 2 * k; j++)
+            {
+                lowerArray[j] = numbersArr[k + j];
+            }
+
+            for (int m = 0; m < 2 * k; m++)
+            {
+                sum[m] = upperArray[m] + lowerArray[m];
+            }
+            Console.WriteLine( string.Join(" ", sum));
         }
     }
 }
