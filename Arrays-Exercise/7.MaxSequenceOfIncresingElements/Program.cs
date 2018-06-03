eda6ddc80b961c8e393d3f407b1a3b2508918f8a
plamen1982 <plamen.hristov@ymail.com>
2018-06-03 15:09:03 +0600

    7.Max sequence of increasing elements - Arrays

---
+++ Arrays-Exercise/7.MaxSequenceOfIncresingElements/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+
+namespace _7.MaxSequenceOfIncresingElements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+
+            int count = 1;
+            int biggestCount = 0;
+            int startIndex = 0;
+
+            for (int i = 1; i < numbers.Length; i++)
+            {
+                if (numbers[i] > numbers[i - 1])
+                {
+                    count++;
+                    if (biggestCount < count)
+                    {
+                        biggestCount = count;
+                        startIndex = i - biggestCount + 1;
+                    }
+                }
+                else
+                {
+                    count = 1;
+                }
+            }
+            for (int i = startIndex; i < startIndex + biggestCount; i++)
+            {
+                Console.Write($"{numbers[i]} ");
+            }
+        }
+    }
+}
