6d5af5c55627b9f70bdcba82a3db767db75e6a27
plamen1982 <plamen.hristov@ymail.com>
2018-06-03 16:28:36 +0600

    10. Pairs by Difference - Arrays

---
+++ Arrays-Exercise/10.PairsOfDifference/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+
+namespace _10.PairsOfDifference
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+            int difference = int.Parse(Console.ReadLine());
+            int count = 0;
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                for (int j = i; j < numbers.Length; j++)
+                {
+                    if (difference == Math.Abs(numbers[i] - numbers[j]))
+                    {
+                        count++;
+                    }
+                }
+            }
+            Console.WriteLine(count);
+        }
+    }
+}
