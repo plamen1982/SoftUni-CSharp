a1cab8fcb348579e7d818dc6b9dd35474aee7ddd
plamen1982 <plamen.hristov@ymail.com>
2018-06-03 15:59:58 +0600

    9.Index of Letters - Arrays

---
+++ Arrays-Exercise/9.IndexOfLetters/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+
+namespace _9.IndexOfLetters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string letters = Console.ReadLine().ToLower();
+            for (int i = 0; i < letters.Length; i++)
+            {
+                Console.WriteLine($"{letters[i]} -> {(int)letters[i] - 97}");
+            }
+        }
+    }
+}
