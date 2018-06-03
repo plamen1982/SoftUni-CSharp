f6a04450374eecc59b3f7b307e880fff5f75d952
plamen1982 <plamen.hristov@ymail.com>
2018-06-03 15:48:22 +0600

    7.Most Frequent Number - Arrays

---
+++ Arrays-Exercise/8.MostFrequentNumber/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+
+namespace _8.MostFrequentNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+            int number = 0;
+            int biggestCount = int.MinValue;
+            int count = 1;
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                for (int j = 0; j < numbers.Length; j++)
+                {
+                    if (numbers[i] == numbers[j])
+                    {
+                        count++;
+                    }
+                }
+
+                if (biggestCount < count)
+                {
+                    biggestCount = count;
+                    number = numbers[i];
+                }
+                count = 1;
+            }
+            Console.WriteLine(number);
+        }
+    }
+}
