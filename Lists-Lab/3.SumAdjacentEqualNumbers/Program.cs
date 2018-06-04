59bd8cab7e30ec7861b63c8c796001ffb9ac7aea
plamen1982 <plamen.hristov@ymail.com>
2018-06-05 00:07:17 +0600

    Sum adjacent equal numbers - List-Lab

---
+++ Lists-Lab/3.SumAdjacentEqualNumbers/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _3.SumAdjacentEqualNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine().Split(" ").Select(int.Parse).ToList();
+            int tempNumber = 0;
+
+            for (int i = 1; i < numbers.Count; i++)
+            {
+                if (numbers[i] == numbers[i - 1])
+                {
+                    tempNumber = numbers[i] + numbers[i - 1];
+                    numbers[i - 1] = tempNumber;
+                    numbers.RemoveAt(i);
+                    i = 0;
+                }
+            }
+            Console.WriteLine(string.Join(" ", numbers));
+        }
+    }
+}
