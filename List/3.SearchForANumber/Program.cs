08cc077246e1f9d0ee2ee3db829bd5c69e3220d7
plamen1982 <plamen.hristov@ymail.com>
2018-06-05 20:41:46 +0600

    03 Search for a number - List

---
+++ List/3.SearchForANumber/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _3.SearchForANumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine().Split(" ").Select(int.Parse).ToList();
+            int[] conditions = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+
+            List<int> newNumbers = new List<int>();
+
+
+            int takeNums = conditions[0];
+            int deleteNumbs = conditions[1];
+            int searchNums = conditions[2];
+
+
+            for (int i = 0; i < takeNums; i++)
+            {
+                newNumbers.Add(numbers[i]);
+            }
+
+            for (int j = 0; j < deleteNumbs; j++)
+            {
+                newNumbers.RemoveAt(0);
+            }
+
+            if (newNumbers.Contains(searchNums))
+            {
+                Console.WriteLine("YES!");
+            }
+            else
+            {
+                Console.WriteLine("NO!");
+            }
+
+        }
+    }
+}
