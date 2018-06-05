d8db70de64f5d66589364d6f505363923fe7d1b7
plamen1982 <plamen.hristov@ymail.com>
2018-06-05 18:37:21 +0600

    1.Max Sequence of Equal Elements - List

---
+++ List/1.MaxSequenceOfEqualElements/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _1.MaxSequenceOfEqualElements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine().Split(" ").Select(int.Parse).ToList();
+            int currentLenght = 1;
+            int bestLength = 1;
+            int bestIndex = 0;
+
+            for (int i = 1; i < numbers.Count; i++)
+            {
+                if (numbers[i] == numbers[i - 1])
+                {
+                    currentLenght++;
+                    if (bestLength < currentLenght)
+                    {
+                        bestLength = currentLenght;
+                        bestIndex = i;
+                    }
+                }
+                else
+                {
+                    currentLenght = 1;
+                }
+            }
+
+            for (int j = bestIndex; j > bestIndex - bestLength; j--)
+            {
+                Console.Write($"{numbers[j]} ");
+            }
+
+        }
+    }
+}
--- Lists-Lab/3.SumAdjacentEqualNumbers/Program.cs
+++ Lists-Lab/3.SumAdjacentEqualNumbers/Program.cs
@@ -8,17 +8,15 @@ namespace _3.SumAdjacentEqualNumbers
     {
         static void Main(string[] args)
         {
-            List<int> numbers = Console.ReadLine().Split(" ").Select(int.Parse).ToList();
-            int tempNumber = 0;
+            List<double> numbers = Console.ReadLine().Split(" ").Select(double.Parse).ToList();
 
-            for (int i = 1; i < numbers.Count; i++)
+            for (int i = 0; i < numbers.Count - 1; i++)
             {
-                if (numbers[i] == numbers[i - 1])
+                if (numbers[i] == numbers[i + 1])
                 {
-                    tempNumber = numbers[i] + numbers[i - 1];
-                    numbers[i - 1] = tempNumber;
-                    numbers.RemoveAt(i);
-                    i = 0;
+                    numbers[i] = numbers[i] * 2;
+                    numbers.RemoveAt(i + 1);
+                    i = -1;
                 }
             }
             Console.WriteLine(string.Join(" ", numbers));
