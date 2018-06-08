7322eddb981d1975ed5d793805368fd43d8623a5
plamen1982 <plamen.hristov@ymail.com>
2018-06-09 01:05:50 +0900

    3.A Miner Task - DictionaryLinqAndLambda

---
+++ DictionariesLinqAndLambda/3.AMinerTask/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace _3.AMinerTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, int> resourceQuantity = new Dictionary<string, int>();
+
+            while (true)
+            {
+                var resources = Console.ReadLine();
+                if (resources == "stop")
+                {
+                    break;
+                }
+                int quantity = int.Parse(Console.ReadLine());
+
+                if (resourceQuantity.ContainsKey(resources))
+                {
+                    resourceQuantity[resources] += quantity;
+                }
+                else
+                {
+                    resourceQuantity[resources] = quantity;
+                }
+            }
+
+            foreach (KeyValuePair<string, int> item in resourceQuantity)
+            {
+                Console.WriteLine("{0} -> {1}", item.Key, item.Value);
+            }
+        }
+    }
+}
