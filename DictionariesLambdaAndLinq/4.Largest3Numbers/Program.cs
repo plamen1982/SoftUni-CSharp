317a77ea364eef786ea97ef3f9660760ff74d28f
plamen1982 <plamen.hristov@ymail.com>
2018-06-08 07:42:43 +0600

    Short Words Sorted - DictionariesLAMBDALINQ-LAB

---
+++ DictionariesLambdaAndLinq/4.Largest3Numbers/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _4.Largest3Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> inputNumbers = Console.ReadLine().Split().Select(int.Parse).ToList();
+            int[] result = inputNumbers.OrderByDescending(x => x).Take(3).ToArray();
+            Console.WriteLine(string.Join(" ", result));
+
+        }
+    }
+}
---
+++ DictionariesLambdaAndLinq/5.ShortWordsSorted/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+namespace _5.ShortWordsSorted
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] words = Console.ReadLine().ToLower().Split(new char[] { '.', ',', ':', ';', '(', ')', '[', ']', '"', '\'', '\\', '/', '!', '?', ' ' }, StringSplitOptions.RemoveEmptyEntries);
+            string[] result = words.Where(x => x.Length < 5)
+                  .OrderBy(x => x)
+                  .Distinct()
+                  .ToArray();
+            Console.WriteLine(string.Join(", ", result));
+        }
+    }
+}
