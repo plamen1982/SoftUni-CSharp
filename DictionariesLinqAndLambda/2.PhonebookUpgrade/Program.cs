badf0c81e0c7af357c16bb6b6111cf95487ff95a
plamen1982 <plamen.hristov@ymail.com>
2018-06-08 15:57:40 +0600

    1.Phonebook Upgrade - DictionaryLinqAndLambda

---
+++ DictionariesLinqAndLambda/2.PhonebookUpgrade/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _2.PhonebookUpgrade
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SortedDictionary<string, string> phonebook = new SortedDictionary<string, string>();
+            while (true)
+            {
+                string line = Console.ReadLine();
+                if (line == "END")
+                {
+                    break;
+                }
+
+                if (line != "ListAll")
+                {
+                    string[] tokens = line.Split();
+                    string command = tokens[0];
+                    string name = tokens[1];
+
+                    switch (command)
+                    {
+                        case "A":
+                            if (phonebook.ContainsKey(name))
+                            {
+                                phonebook[name] = tokens[2];
+                            }
+                            else
+                            {
+                                phonebook.Add(name, tokens[2]);
+                            }
+                            break;
+                        case "S":
+                            if (phonebook.ContainsKey(name))
+                            {
+                                Console.WriteLine($"{name} -> {phonebook[name]}");
+                            }
+                            else
+                            {
+                                Console.WriteLine($"Contact {name} does not exist.");
+                            }
+                            break;
+
+                        default:
+                            break;
+                    }
+                }
+                else
+                {
+                    foreach (var phone in phonebook)
+                    {
+                        Console.WriteLine($"{phone.Key} -> {phone.Value}");
+                    }
+                }
+            }
+        }
+    }
+}
