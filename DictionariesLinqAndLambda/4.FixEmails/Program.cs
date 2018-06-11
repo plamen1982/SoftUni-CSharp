8b6a0c42b0c3faf98171753f10db63572ed0f394
plamen1982 <plamen.hristov@ymail.com>
2018-06-11 16:54:26 +0900

    4. Fix Emails - DictionaryLinqAndLambda

---
+++ DictionariesLinqAndLambda/4.FixEmails/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _4.FixEmails
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, string> persons = new Dictionary<string, string>();
+
+            while (true)
+            {
+                string name = Console.ReadLine();
+                if (name == "stop")
+                {
+                    break;
+                }
+                string email = Console.ReadLine();
+                if (isFromUsOrUk(email))
+                {
+                    continue;
+                }
+                else
+                {
+                    persons[name] = email;
+                }
+
+            }
+
+            foreach (var person in persons)
+            {
+                Console.WriteLine($"{person.Key} -> {person.Value}");
+            }
+        }
+
+        static bool isFromUsOrUk(string email)
+        {
+            string usOrUk = string.Empty;
+
+            for (int i = email.Length - 2; i <= email.Length - 1; i++)
+            {
+                usOrUk += string.Empty + email[i];
+            }
+            if (usOrUk.ToLower().Equals("us") || usOrUk.ToLower().Equals("us"))
+            {
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
