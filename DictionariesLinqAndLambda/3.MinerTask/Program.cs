52abaa519d597c10672025815963830f80cb36cc
plamen1982 <plamen.hristov@ymail.com>
2018-06-11 16:22:46 +0900

    3. Miner Collection - DictionaryLinqAndLambda

---
+++ DictionariesLinqAndLambda/3.MinerTask/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _3.MinerTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Dictionary<string, long> minerCollection = new Dictionary<string, long>();  
+
+            while (true)
+            {
+                string mineral = Console.ReadLine();
+                if (mineral == "stop")
+                {
+                    break;
+                }
+                long value = long.Parse(Console.ReadLine());
+
+                if (minerCollection.ContainsKey(mineral))
+                {
+                    minerCollection[mineral] += value;
+                }
+                else
+                {
+                    minerCollection[mineral] = value;
+                }
+            }
+            foreach (KeyValuePair<string, long> miner in minerCollection)
+            {
+                Console.WriteLine($"{miner.Key} -> {miner.Value}");
+            }
+
+        }
+    }
+}
