680b69ff87f024a29360a62e0e293cd72b05d340
plamen1982 <plamen.hristov@ymail.com>
2018-06-11 15:39:33 +0900

    5.Hands Of Cards - made it with Dictionary - DictionaryLinqAndLambda

--- DictionariesLinqAndLambda/5.HandsOfCards/Program.cs
+++ DictionariesLinqAndLambda/5.HandsOfCards/Program.cs
@@ -8,36 +8,101 @@ namespace _5.HandsOfCards
     {
         static void Main(string[] args)
         {
-            Dictionary<string, decimal> users = new Dictionary<string, decimal>();
+            Dictionary<string, List<string>> users = new Dictionary<string, List<string>>();
             while (true)
             {
-
-                string[] tokens = Console.ReadLine().Split(":,".ToCharArray()).Distinct().ToArray();
+                string[] tokens = Console.ReadLine().Split(":").ToArray();
                 string name = tokens[0];
+
                 if (name == "JOKER")
                 {
                     break;
                 }
-                for (int i = 1; i < tokens.Length; i++)
+                List<string> playingCards = tokens[1].Trim().Split(", ").ToList();
+
+
+                if (users.ContainsKey(name) == false)
                 {
-                    if (users.ContainsKey(name))
+                    users[name] = new List<string>();
+                }
+                    users[name].AddRange(playingCards);
+            }
+
+            Dictionary<string, int> cards = new Dictionary<string, int>();
+
+            // string[] cardsToSwitch = tokens[1].Trim().Split(", ").ToArray();
+
+            for (int i = 2; i <= 9; i++)
+            {
+                cards.Add(i.ToString(), i);
+            }
+
+            cards.Add("1", 10);
+            cards.Add("J", 11);
+            cards.Add("Q", 12);
+            cards.Add("K", 13);
+            cards.Add("A", 14);
+            cards.Add("S", 4);
+            cards.Add("H", 3);
+            cards.Add("D", 2);
+            cards.Add("C", 1);
+
+
+            foreach (var user in users)
+            {
+                Dictionary<string, List<string>> distinctPlayers = new Dictionary<string, List<string>>();
+                distinctPlayers.Add(user.Key, user.Value.Distinct().ToList());
+
+                int sum = 0;
+
+                foreach (var player in distinctPlayers)
+                {
+                    foreach (var card in player.Value)
                     {
-                        users[name] += CalculateValue(tokens[i]);
+                        string[] currentCard = ReturnCardsToCalculate(card);
+                        int typeCard = cards[currentCard[0]];
+                        int colorCard = cards[currentCard[1]];
+
+                        sum += typeCard * colorCard;
+                        
                     }
-                    else
-                    {
-                        users.Add(name, CalculateValue(tokens[i]));
-                    }
+
+                    Console.WriteLine($"{player.Key}: {sum}");
+                    sum = 0;
                 }
 
             }
-            foreach (var user in users)
-            {
-                Console.WriteLine($"{user.Key}: {user.Value}");
-            }
-        }
-        //Pesho: 2C, 4H, 9H, AS, QS
 
+
+
+        }
+        static string[] ReturnCardsToCalculate(string card)
+        {
+
+            string typeCard;
+            string typeColor;
+            string[] result = new string[2];
+
+            if (card[0] == '1')
+            {
+                 typeCard = card[0].ToString();
+                 typeColor = card[2].ToString();
+            }
+            else
+            {
+                 typeCard = card[0].ToString();
+                 typeColor = card[1].ToString();
+            }
+
+
+            result[0] = typeCard;
+            result[1] = typeColor;
+            return result;
+        }
+
+
+        //Pesho: 2C, 4H, 9H, AS, QS
+        /*
         static decimal CalculateValue(string combination)
         {
             string firstElement = "" + combination[1];
@@ -107,6 +172,7 @@ namespace _5.HandsOfCards
             }
             return firstInt * secondInt;
         }
+        */
 
     }
 }
