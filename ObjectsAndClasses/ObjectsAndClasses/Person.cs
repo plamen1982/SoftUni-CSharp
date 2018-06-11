bbf75fecbc48243cacb4a7989d4e27e876c74aa7
plamen1982 <plamen.hristov@ymail.com>
2018-06-11 20:02:01 +0900

    Classes and Objects

---
+++ ObjectsAndClasses/ObjectsAndClasses/Person.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ObjectsAndClasses
+{
+    class Person
+    {
+        private string name;
+        private int age;
+
+
+
+        public string getName()
+        {
+            return this.name;
+        }
+
+        public void setName(string nameInput)
+        {
+            this.name = nameInput;
+        }
+
+        public Person(string name, int age)
+        {
+            this.age = age;
+            this.name = name;
+        }
+
+        public Person()
+        {
+            //default constructor
+        }
+
+    }
+}
---
+++ ObjectsAndClasses/ObjectsAndClasses/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ObjectsAndClasses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person(name: "Pesho", age: 29);
+            Person person1 = new Person();
+
+            Console.WriteLine(person.getName());
+
+        }
+    }
+}
