ad11ae8a36a1caee9ccaf2521fd5de363835b03b
plamen1982 <plamen.hristov@ymail.com>
2018-06-14 19:29:24 +0900

    1.Reverse String - Strings And Text Processing

---
+++ StringsAndTextProcessing/1.ReverseString/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+
+namespace _1.ReverseString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            string reversedString = ReverseString(input);
+            Console.WriteLine(reversedString);
+
+        }
+
+        static string ReverseString(string input)
+        {
+            return string.Join("",input.ToCharArray().Reverse());
+        }
+    }
+}
