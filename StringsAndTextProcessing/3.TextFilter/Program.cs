59be51de0073ba4d3db09b054bf80aa630e5b715
plamen1982 <plamen.hristov@ymail.com>
2018-06-15 13:41:07 +0900

    3.Text Filter with Aggregate function - Strings And Text Processing

--- StringsAndTextProcessing/3.TextFilter/Program.cs
+++ StringsAndTextProcessing/3.TextFilter/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using System.Text;
 
 namespace _3.TextFilter
@@ -7,10 +8,13 @@ namespace _3.TextFilter
     {
         static void Main(string[] args)
         {
-            string[] bannedWords = Console.ReadLine().Split(',', ' ', StringSplitOptions.RemoveEmptyEntries);
 
-            
+            //string[] bannedWords = Console.ReadLine().Split(',', ' ', StringSplitOptions.RemoveEmptyEntries);
             //string[] bannedWords = Console.ReadLine().Split(", ".ToCharArray(), StringSplitOptions.RemoveEmptyEntries);
+
+            /*
+             * First Solution
+            string[] bannedWords = Console.ReadLine().Split(',').Select(x => x.Trim()).ToArray();
             string textToBeChecked = Console.ReadLine();
             for (int i = 0; i < bannedWords.Length; i++)
             {
@@ -29,5 +33,17 @@ namespace _3.TextFilter
 
             return result;
         }
+        */
+
+            //Second Solution
+
+            string[] banList = Console.ReadLine()
+                .Split(new[] { ',', ' ' }, StringSplitOptions.RemoveEmptyEntries);
+            //textToCkeck is the text from Console.Readline(), the previous argument. banWord is every element from banList[] array
+            string text = banList.Aggregate(Console.ReadLine(), (textToCheck, banWord) => textToCheck.Replace(banWord, new string('*', banWord.Length)));
+
+            Console.WriteLine(text);
+        }
+        
     }
 }
