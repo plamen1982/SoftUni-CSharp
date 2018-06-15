1d27678a7b0bdd98a95502d543b1bc9f273fa07a
plamen1982 <plamen.hristov@ymail.com>
2018-06-15 14:06:10 +0900

    4.Palindromes - Strings And Text Processing

---
+++ StringsAndTextProcessing/4.Palindromes/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _4.Palindromes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] text = Console.ReadLine().Split(",?!. ".ToCharArray(), StringSplitOptions.RemoveEmptyEntries);
+            List<string> result = new List<string>();
+
+            foreach (var word in text)
+            {
+                if (IsPalindrom(word))
+                {
+                    result.Add(word);
+                }
+            }
+            Console.WriteLine(string.Join(", " , result.Distinct().OrderBy(x => x)));
+        }
+
+        static bool IsPalindrome(string word)
+        {
+            bool isPalindrom = false;
+            for (int i = 0; i < word.Length / 2; i++)
+            {
+                if (word[i] != word[word.Length - 1 - i])
+                {
+                    return isPalindrom = false;
+                }
+            }
+            return isPalindrom = true;
+        }
+    }
+}
