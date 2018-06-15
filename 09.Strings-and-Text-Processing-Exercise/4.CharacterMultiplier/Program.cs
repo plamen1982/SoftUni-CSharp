ba3c869d549f30be5ddc5dea973674a69cdae1f0
plamen1982 <plamen.hristov@ymail.com>
2018-06-15 20:16:11 +0900

    4. Character Multiplier - Strings-and-Text

---
+++ 09.Strings-and-Text-Processing-Exercise/4.CharacterMultiplier/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+
+namespace _4.CharacterMultiplier
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine()
+                .Split()
+                .OrderByDescending(c => c.Length)
+                .ToArray();
+
+            string string1 = input[0];
+            string string2 = input[1];
+
+            long sum = 0;
+
+            for (int j = 0; j < string2.Length; j++)
+            {
+                sum += string1[j] * string2[j];
+            }
+
+            for (int i = string2.Length; i < string1.Length; i++)
+            {
+                sum += string1[i];
+            }
+
+            Console.WriteLine(sum); 
+        }
+    }
+}
