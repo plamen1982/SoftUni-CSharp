d8f542a2685e759faecd91cf5c5f45a3bf5107da
plamen1982 <plamen.hristov@ymail.com>
2018-06-15 13:07:06 +0900

    3.Text Filter - Strings And Text Processing

---
+++ StringsAndTextProcessing/3.TextFilter/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Text;
+
+namespace _3.TextFilter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] bannedWords = Console.ReadLine().Split(',', ' ', StringSplitOptions.RemoveEmptyEntries);
+
+            
+            //string[] bannedWords = Console.ReadLine().Split(", ".ToCharArray(), StringSplitOptions.RemoveEmptyEntries);
+            string textToBeChecked = Console.ReadLine();
+            for (int i = 0; i < bannedWords.Length; i++)
+            {
+                textToBeChecked = textToBeChecked.Replace(bannedWords[i], Repeat("*", bannedWords[i].Length));
+            }
+            Console.WriteLine(textToBeChecked);
+        }
+
+        static string Repeat(string word, int timesToBeRepeated)
+        {
+            string result = string.Empty;
+            for (int i = 0; i < timesToBeRepeated; i++)
+            {
+                result += word;
+            }
+
+            return result;
+        }
+    }
+}
