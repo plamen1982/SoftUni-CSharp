1005517a72dc368fc6b0a472d8a01b3d79cabd1f
plamen1982 <plamen.hristov@ymail.com>
2018-06-18 15:48:35 +0900

    Match Full Name - RexEx

---
+++ RegularExpressions-Lab/1.MatchFullName/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace _1.MatchFullName
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string pattern = @"\b[A-Z][a-z]+\s[A-Z][a-z]+\b";
+            Regex regex = new Regex(pattern);
+            string names = Console.ReadLine();
+            foreach (Match match in regex.Matches(names))
+            {
+                Console.Write(match.Value + " ");
+            }
+        }
+    }
+}
