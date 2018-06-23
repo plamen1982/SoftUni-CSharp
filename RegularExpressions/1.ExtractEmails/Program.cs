fad6371e22ea62246444a2743f21584c114fd680
plamen1982 <plamen.hristov@ymail.com>
2018-06-23 11:40:57 +0900

    Extract Emails - Regular Expressions(RegEx)

---
+++ RegularExpressions/1.ExtractEmails/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace _1.ExtractEmails
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string pattern = @"(\b[\w.-]+@[a-zA-z-]+)(\.[a-zA-z-]+)+";
+            string pattern = @"(?:[A-Za-z0-9]+)([_.-]\w*)*@([A-Za-z_.-]+)+\b";
+            Regex rgxEmail = new Regex(pattern);
+            string inputText = Console.ReadLine();
+
+            foreach (Match emailMatch in rgxEmail.Matches(inputText))
+            {
+                Console.WriteLine(emailMatch.Value);
+            }
+        }
+    }
+}
---
+++ RegularExpressions/3.CameraView/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq;
+
+namespace _3.CameraView
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] elements = Console.ReadLine().Split().Select(x => int.Parse(x)).ToArray();
+            string text = Console.ReadLine();
+
+
+        }
+    }
+}
