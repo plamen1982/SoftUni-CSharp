e495f70ae2ec18bcfe7a7398ae79e680e60e08d7
plamen1982 <plamen.hristov@ymail.com>
2018-06-26 07:33:36 +0900

    Rage Quit

---
+++ Exam-Preparation/3.RageQuit/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace _3.RageQuit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+            string pattern = @"(\D+)(\d+)";
+            Regex rgx = new Regex(pattern);
+            StringBuilder result = new StringBuilder();
+
+            foreach (Match match in rgx.Matches(text))
+            {
+                for (int i = 0; i < int.Parse(match.Groups[2].Value); i++)
+                {
+                    result.Append(match.Groups[1].Value.ToUpper());
+                }
+            }
+
+            long count = result.ToString().Distinct().Count();
+            Console.WriteLine($"Unique symbols used: {count}");
+            Console.WriteLine(result.ToString());
+
+        }
+    }
+}
