18c706a974eebb4f8f87b8735e645531759c1f20
plamen1982 <plamen.hristov@ymail.com>
2018-06-26 11:13:16 +0900

    4.Files

---
+++ Exam-Preparation/Files/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Files
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+          
+            Dictionary<string, Dictionary<string, long>> rootsExtentionsAndSizes = new Dictionary<string, Dictionary<string, long>>();
+            while (n-- > 0)
+            {
+                //Windows\Temp\win.exe;5423 - test
+                string[] line = Console.ReadLine().Split("\\");
+                string root = line[0];
+                string fileNameAndSize= line[line.Length - 1];
+                string fileName = fileNameAndSize.Split(";")[0];
+                long size = long.Parse(fileNameAndSize.Split(";")[1]);
+
+                //does not exist
+                if (rootsExtentionsAndSizes.ContainsKey(root) == false)
+                {
+                    rootsExtentionsAndSizes.Add(root, new Dictionary<string, long>());
+                    if (rootsExtentionsAndSizes[root].ContainsKey(fileName) == false)
+                    {
+                        rootsExtentionsAndSizes[root].Add(fileName, size);
+                    }
+                    else
+                    {
+                        rootsExtentionsAndSizes[root][fileName] = size;
+                    }
+                }
+                else
+                {
+                    if (rootsExtentionsAndSizes[root].ContainsKey(fileName) == false)
+                    {
+                        rootsExtentionsAndSizes[root].Add(fileName, size);
+                    }
+                    else
+                    {
+                        rootsExtentionsAndSizes[root][fileName] = size;
+                    }
+                }
+            }
+
+            string[] output = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+            string extentionOutput = output[0];
+            string rootOutput = output[2];
+            bool isExist = false;
+            bool isPrint = false;
+            //rootsExtentionsAndSizes = [Games: {keygen.exe: 1024, wow.exe: 1024}, Windows : { someKey: someVaue}  }]; output = "Games"
+            if (rootsExtentionsAndSizes.ContainsKey(rootOutput))
+            {
+
+                var result = rootsExtentionsAndSizes.Where(x => x.Key.Contains(rootOutput)).ToDictionary(x => x.Key, v => v.Value);
+                foreach (var filesSizes in result.Values)
+                {
+                    //filesSizes = [{keygen.exe: 1024}, {k:v}]
+                    //fileSize k:v
+                    foreach (var fileSize in filesSizes.Where(x => x.Key.Contains(extentionOutput)).ToDictionary(x => x.Key, v => v.Value))
+                    {
+                        //k : "string"
+                        if (fileSize.Key.Contains($".{extentionOutput}") == false)
+                        {
+                            isExist = false;
+                            continue;
+                        }
+                        else
+                        {
+                            Console.WriteLine($"{fileSize.Key} - {fileSize.Value} KB");
+                            isExist = true;
+                            isPrint = true;
+                        }
+                    }
+                } 
+            }
+            else
+            {
+                Console.WriteLine("No");
+            }
+            if (isPrint == false && isExist == false)
+            {
+                Console.WriteLine("No");
+            }
+        }
+    }
+}
--- Exam-Preparation/Test/Program.cs
+++ Exam-Preparation/Test/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace Test
@@ -9,11 +10,64 @@ namespace Test
         static void Main(string[] args)
         {
 
-            StringBuilder merged = new StringBuilder();
-            merged.Append("First");
-            merged.Append("Second");
-            Console.WriteLine(merged.ToString());
+            string[] windows = { "Windows", "Windows", "Windows", "Games", "Games", "OtherGames", "OtherGames" };
+            string games = "Games";
+            List<string> fileName1 = new List<string> { "keygen.exe", "wow.exe", "keygen.exe", "wow.exe", "keygen.exe" };
+            List<long> size1 = new List<long> { 1024, 2056, 4128, 8256, 16512 };
 
+            Dictionary<string, Dictionary<string, long>> filesNamesAndSizes = new Dictionary<string, Dictionary<string, long>>();
+            // windows = new Dict()
+            //{windows : {kyegen.exe, 1024, wow.exe, 2056}}
+            for (int i = 0; i < 5; i++)
+            {
+                // Check if windows exist, 
+                if (filesNamesAndSizes.ContainsKey(windows[i]) == false)
+                {
+                    //create Dict, and add key, value to this new dict with key windows
+                    filesNamesAndSizes[windows[i]] = new Dictionary<string, long>();
+                    if (filesNamesAndSizes[windows[i]].ContainsKey(fileName1[i]) == false)
+                    {
+                        //if key for the inner dict does not exist create new pair key, value
+                        filesNamesAndSizes[windows[i]].Add(fileName1[i], size1[i]);
+
+                    }
+
+                    else
+                    {   // if the key for the inner dict exist, override the value, or if you want accumulate it with +=
+                        filesNamesAndSizes[windows[i]][fileName1[i]] = size1[i];
+                    }
+                }
+                else
+                {
+                    // if exist, check if there is a key existing agains windows key in the other dictionary,
+                    if (filesNamesAndSizes[windows[i]].ContainsKey(fileName1[i]) == false)
+                    {
+                        //if does not exist, create such a such a key, with size
+                        filesNamesAndSizes[windows[i]].Add(fileName1[i], size1[i]);
+                    }
+
+                    else
+                    {
+                        //if it not exist add the value to this key, 
+                        filesNamesAndSizes[windows[i]][fileName1[i]] += size1[i];
+                        //or delete the existing value with a new one
+                        //filesNamesAndSizes[windows[i]][fileName1[i]] = size1[i];
+                    }
+
+                }
+            }
+
+
+            foreach (var folder in filesNamesAndSizes.Values)
+            {
+                foreach (var fileNameAndSize in folder.OrderByDescending(x => x.Value).ThenBy(x => x.Key))
+                {
+                    Console.WriteLine($"{fileNameAndSize.Key} - {fileNameAndSize.Value} KB");
+                }
+            }
+
+            List<string> someStr = new List<string> { "List1Element", "List1Element" };
+            someStr.fo
         }
     }
 }
